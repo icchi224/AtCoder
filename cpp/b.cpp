a2e5088cbe9e59844917f6d2d6d2c029ec5f40b3
icchi <hiroaki.i.6501@gmail.com>
2023-04-01 14:52:23 +0900

    4/1

--- cpp/b.cpp
+++ cpp/b.cpp
@@ -1,11 +1,8 @@
-#include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define all(x) std::begin(x), std::end(x)
+#include <iostream>
 using namespace std;
-using ll = long long;
-using P = pair<int, int>;
 
 int main() {
-    vector<int> a = {1,2,3};
-    set<int> 
+    int a;
+    cin >> a;
+    cout << a-1 << endl;
 }
